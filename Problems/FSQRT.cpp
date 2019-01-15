c9d1f29621c3076e7a137ffb3a4b4e5c4110a476
Jonorsky <jonavarrete@gbox.adnu.edu.ph>
2019-01-15 23:57:04 +0800

    Added FSQRT

---
+++ Problems/FSQRT.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+
+	double t,n;
+	cin >> t;
+	while(t--) {
+		cin >> n;
+		cout << int(sqrt(n)) << endl;
+	}
+
+	return 0;
+}

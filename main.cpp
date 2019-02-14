dbb09966f5ba200feb50c28f4dcaf4c09e8567e5
Jonorsky <jonavarrete@gbox.adnu.edu.ph>
2019-02-14 23:49:55 +0800

    Added Function

--- main.cpp
+++ main.cpp
@@ -1,24 +1,12 @@
 #include <iostream>
-#include <vector>
-#include <map>
-
 using namespace std;
 
+void swap(int *xp, int*yp){
+  
+}
+
 int main(){
-  map<string,int> m_map;
-  map<string,int>::iterator it;
-  m_map["Hello"]++;
 
-  cout << m_map["Hello"] << endl;
-
-  map<char,int> maps;
-  for(int i=0; i<5; i++){
-    maps[char(i+95)]++;
-  }
-
-  for(it=m_map.begin(); it!=m_map.end(); it++){
-    cout << it->first << ' ' << it->second << endl;
-  }
 
   return 0;
 }

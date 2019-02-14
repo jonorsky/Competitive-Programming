84eb05c6704ae61071968082758ecec734af543b
Jonorsky <jonavarrete@gbox.adnu.edu.ph>
2019-02-14 23:53:51 +0800

    Added content of loop

--- main.cpp
+++ main.cpp
@@ -10,7 +10,10 @@ void swap(int *xp, int*yp){
 void selectionSort(int arr[],int n){
   int i,j,min_idx;
   for(i = 0; i<n-1; i++){
-
+    min_idx = i;
+       for (j = i+1; j < n; j++)
+         if (arr[j] < arr[min_idx])
+           min_idx = j; 
   }
 }
 

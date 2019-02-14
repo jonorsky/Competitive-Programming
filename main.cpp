30b2699cbd2953781446ff2f5a43192d62527f89
Jonorsky <jonavarrete@gbox.adnu.edu.ph>
2019-02-14 23:54:40 +0800

    Main content

--- main.cpp
+++ main.cpp
@@ -23,10 +23,15 @@ void printArray(int arr[], int size)
     for (i=0; i < size; i++)
         printf("%d ", arr[i]);
     printf("\n");
-} 
+}
 
 int main(){
-
+   int arr[] = {64, 25, 12, 22, 11};
+   int n = sizeof(arr)/sizeof(arr[0]);
+   selectionSort(arr, n);
+   printf("Sorted array: \n");
+   printArray(arr, n);
+   return 0;
 
   return 0;
 }

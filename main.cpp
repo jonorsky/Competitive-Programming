1f0526ec12414598b43d1cdd74737e5a7ea1b9d6
Jonorsky <jonavarrete@gbox.adnu.edu.ph>
2019-02-18 14:55:58 +0800

    Initialize stack

--- main.cpp
+++ main.cpp
@@ -1,39 +1,7 @@
 #include <iostream>
-#include <queue>
 using namespace std;
 
 int main(){
-  struct Node{
-    int key;
-    struct Node *left, *right;
-  };
-
-  struct Node *newNode(int key){
-    struct Node* temp = new Node;
-    temp->key = key;
-    temp->left = temp->right = NULL;
-    return temp;
-  }
-
-  void inorder(struct Node *temp){
-    if(!temp) return;
-    inorder(temp->left);
-    cout << temp->key << " ";
-    inorder(temp->right);
-  }
-
-  void insert(struct Node*temp, int key){
-    queue<struct Node*>q;
-    q.push(temp);
-
-    while(!q.empty()){
-      struct Node* temp = q.front();
-      q.pop();
-
-      if(!temp->left){
-        temp->left = newNode(key);
-      }
-    }
-  }
-  return 0;
+	return 0;
+	
 }
